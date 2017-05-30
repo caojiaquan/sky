95caedf312e7cf9d744afbf851dc23fd2672ce7e
caojiaquan <cjq95613>
2017-05-30 20:24:48 +0800

    music vue  file

---
+++ components/Nav.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="nav">
+        <h1><router-link to="/main" class="nav-font selected">首页</router-link></h1>
+        <h1><router-link to="/second" class="nav-font">推介</router-link></h1>
+        <h1><router-link to="/third" class="nav-font">歌单</router-link></h1>
+        <h1><router-link to="/four" class="nav-font">我的</router-link></h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'nav',
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    @import '../assets/css/common.css';
+    @import '../assets/css/index.css';
+    @import '../assets/css/index2.css';
+</style>
---
+++ components/Third.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <aaa></aaa>
+        <div id="container-third" class="wrapper">
+            <div id="container-third-nav">
+                <input type="text" placeholder="&nbsp;&nbsp;搜索歌曲"/>
+                <img src="../assets/images/search.png" alt=""/>
+            </div>
+            <div id="container-third-banner">
+            </div>
+            <h3 id="container-third-font">全部歌曲</h3>
+            <ul id="container-third-content">
+                <li><img src="" alt=""/><span>songname</span></li>
+                <li><img src="" alt=""/><span>songname</span></li>
+            </ul>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import Nav from './Nav.vue'
+    export default {
+        name: 'hello',
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        },
+        components:{
+            aaa:Nav
+        }
+    }
+</script>
+
+<style>
+
+    @import '../assets/css/common.css';
+    @import '../assets/css/index.css';
+    @import '../assets/css/index2.css';
+</style>
